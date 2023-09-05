38ed8f82be2355f13049b2b95a3986165037363b
David Tei <daveteidt@gmail.com>
2023-09-05 00:42:55 +0000

    update repo class

--- src/git_repo.py
+++ src/git_repo.py
@@ -5,17 +5,58 @@ import os
 
 
 class GitRepo(object):
+    """A class that defines a git repository."""
     # location of the version control files:
     workdir = None
     # where to save the .git dir:
     dotgit = None
+    # the config file:
     config = None
 
     def __init__(self, path, create=False):
+        """Initialize a git repository."""
         self.workdir = path
-        self.dotgit = path + "/.git"
+        self.dotgit = os.path.join(path, ".git")
 
         if not (create or os.path.isdir(self.dotgit)):
-            raise FileNotFoundError(f"fatal: {self.dotgit} is not a git repository")
+            raise FileNotFoundError(
+                f"fatal: {self.dotgit} is not a git repository")
 
         # reading the config file in the .git directory:
+        self.config = configparser.ConfigParser()
+        configfile = git_file_dir(self, "config")
+
+        if configfile and os.path.exists(configfile):
+            self.config.read([configfile])
+        elif not create:
+            raise FileNotFoundError("fatal: config file missing")
+
+        # making sure the version of the repository is supported by the
+        # current version Git:
+        # repositoryformatversion = 0 ==> original/compatible format
+        if not create:
+            version = int(self.config.get("core", "repositoryformatversion"))
+            if version != 0:
+                raise ValueError(
+                    f"fatal: unsupported repositoryformatversion {version}")
+
+
+def git_file_path(repo, *path):
+    """Return the path to a file or directory in the git directory."""
+    return os.path.join(repo.dotgit, *path)
+
+def git_file_dir(repo, *path, create_dir=False):
+    """Return the path to a file or directory in the git directory."""
+    path = repo.git_file_path(*path)
+
+    if os.path.exists(path):
+        if os.path.isdir(path):
+            return path
+    else:
+        raise NotADirectoryError(f"fatal: {path} is not a directory")
+    if create_dir:
+        os.makedirs(path)
+        return path
+    else:
+        return None
+
