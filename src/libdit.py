3fde03fb7d016077e6e0bbea08461b8ea14acb31
David Tei <daveteidt@gmail.com>
2023-09-18 05:14:49 +0000

    Add update-ref

--- src/libdit.py
+++ src/libdit.py
@@ -843,3 +843,42 @@ def dit_show_ref(args):
     repo = find_repo_root()
     refs = list_refs(repo)
     show_ref(repo, refs, prefix="refs/")
+
+
+# dit update-ref: allows updating the references in the repository
+# dit update-ref will be implemented as dit update-ref <ref> <sha>
+# It will instantiate the tree object and write the files to the path provided
+update_ref_arg = subparsers.add_parser(
+    "update-ref",
+    help="Update the object name stored in a ref safely",
+    usage="dit update-ref <ref> <sha>",
+    epilog="See 'dit update-ref --help' for more information on a specific command.")
+
+update_ref_arg.add_argument(
+    "ref",
+    metavar="ref",
+    help="The ref to update")
+
+update_ref_arg.add_argument(
+    "sha",
+    metavar="sha",
+    help="The sha to update the ref to")
+
+
+def update_ref(repo, ref, sha):
+    """Update the object name stored in a ref safely."""
+    # creating the path to the reference:
+    path = git_file_path(repo, ref, create_dir=True)
+
+    # writing the reference to the git repository:
+    with open(path, "w", encoding="utf-8") as f:
+        f.write(sha + "\n")
+
+    # returning the reference:
+    return ref
+
+
+def dit_update_ref(args):
+    """Update the object name stored in a ref safely."""
+    repo = find_repo_root()
+    update_ref(repo, args.ref, args.sha)
