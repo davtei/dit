2fe5c3277191b33022117fbcce7e4efc70184cdb
David Tei <daveteidt@gmail.com>
2023-09-18 05:01:48 +0000

    Add show-ref

--- src/libdit.py
+++ src/libdit.py
@@ -757,3 +757,89 @@ def dit_checkout(args):
     checkout_tree(repo, args.commit, args.path)
 
 
+# References/refs: are files that store access to commit objects
+# Refs contain a 48 byte hex string that is the sha of the commit object
+# Refs may refer to other references
+# Refs are stored in the .git/refs directory
+
+
+def ref_resolver(repo, ref):
+    """Resolve the reference to a commit sha."""
+
+    path = git_file_path(repo, ref)
+
+    # making sure the reference exist:
+    if not os.path.exists(path):
+        raise ValueError(f"{ref} not found")
+
+    # reading the reference without the newline character:
+    with open(path, encoding="utf-8") as f:
+        data = f.read()[:-1]
+
+    # removing the prefix "ref: " from the reference:
+    if data.startswith("ref: "):
+        return ref_resolver(repo, data[5:])
+    else:
+        return data
+
+
+# list_refs: allows listing the references in the repository
+#   as a dictionary of key-value pairs
+def list_refs(repo, path=None):
+    """List the references in the repository as a dictionary of key-value pairs."""
+    # creating the dictionary to store the references:
+    refs = collections.OrderedDict()
+
+    # creating the path to the references:
+    if not path:
+        path = git_file_path(repo, "refs")
+
+    # iterating through the references:
+    for name in os.listdir(path):
+        # creating the path to the reference:
+        fullname = os.path.join(path, name)
+
+        # making sure the reference is not a directory:
+        if os.path.isdir(fullname):
+            refs[name] = list_refs(repo, fullname)
+        else:
+            # reading the reference without the newline character:
+            with open(fullname, encoding="utf-8") as f:
+                refs[name] = f.read()[:-1]
+
+    # returning the dictionary of references:
+    return refs
+
+
+# dit show-ref: allows showing the references in the repository
+# dit show-ref will be implemented as dit show-ref <ref>
+# It will instantiate the tree object and write the files to the path provided
+show_ref_arg = subparsers.add_parser(
+    "show-ref",
+    help="List references in a local repository",
+    usage="dit show-ref <ref>",
+    epilog="See 'dit show-ref --help' for more information on a specific command.")
+
+
+# TODO: dit show-ref implementation is not complete:
+
+# TODO: fix display of output in dit show-ref:
+#     dit show-ref displays the sha and the ref
+
+def show_ref(repo, refs, with_hash=True, prefix=""):
+    """List references in a local repository."""
+    # iterating through the references:
+    for name, ref in refs.items():
+        # making sure the reference is not a directory:
+        if isinstance(ref, collections.OrderedDict):
+            show_ref(repo, ref, with_hash, prefix + name + "/")
+        else:
+            # printing the reference:
+            print(ref, prefix + name)
+
+
+def dit_show_ref(args):
+    """List references in a local repository."""
+    repo = find_repo_root()
+    refs = list_refs(repo)
+    show_ref(repo, refs, prefix="refs/")
