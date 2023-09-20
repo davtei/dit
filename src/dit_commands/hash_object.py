a68fe570956f607890a09cbc56871c365eca53a3
David Tei <daveteidt@gmail.com>
2023-09-20 11:11:59 +0000

    Split init and hash_object from libdit

---
+++ src/dit_commands/hash_object.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""A module that defines the hash-object command.
+Args:
+    file: the file to compute the object ID from.
+    type: the type of the object being written.
+    write: actually write the object into the object database.
+Returns:
+    The object ID.
+Raises:
+    ValueError: if the repository already exists.
+    Usage:
+        dit hash-object [-w] [-t TYPE] <file>
+"""
+
+
+import hashlib
+import zlib
+
+from src.parsers import subparsers
+from src.repos.find_root import find_repo_root
+from src.repos.repo_paths import git_file_path
+
+hash_object_arg = subparsers.add_parser(
+    "hash-object",
+    help="Compute object ID and optionally creates a blob from a file",
+    usage="dit hash-object [-w] [-t TYPE] <file>",
+    epilog="See 'dit hash-object --help' for more information on a specific command.")
+
+hash_object_arg.add_argument(
+    "-w",
+    action="store_true",
+    dest="write",
+    help="Actually write the object into the object database")
+
+hash_object_arg.add_argument(
+    "-t",
+    metavar="type",
+    dest="type",
+    default="blob",
+    choices=["blob", "commit", "tag", "tree"],
+    help="Specify the type of the object being written")
+
+hash_object_arg.add_argument(
+    "file",
+    metavar="file",
+    help="The file to compute the object ID from")
+
+
+def hash_object(repo, data, object_format, write=True):
+    """Compute object ID and optionally creates a blob from a file."""
+    # creating the header:
+    header = f"{object_format} {len(data)}\x00".encode()
+
+    # creating the object:
+    store = header + data
+
+    # creating the sha:
+    sha = hashlib.sha1(store).hexdigest()
+
+    # creating the path to the object:
+    path = git_file_path(repo, "objects", sha[:2], sha[2:],
+                         create_dir=write)
+
+    # writing the object to the git repository:
+    if write:
+        with open(path, "wb") as f:
+            f.write(zlib.compress(store))
+    return sha
+
+
+def dit_hash_object(args):
+    """Compute object ID and optionally creates a blob from a file,
+    that is, converts a file into a git object."""
+    repo = find_repo_root()
+    with open(args.file, "rb") as f:
+        data = f.read()
+    sha = hash_object(repo, data, args.type, args.write)
+    print(sha)
---
+++ src/dit_commands/init.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""A module that defines the init command."""
+
+import sys
+
+from src.parsers import subparsers
+from src.repos.create_repo import create_repo
+
+# Create a subparser for the init command
+init_arg = subparsers.add_parser(
+    "init",
+    help="Initialize a new, empty repository.")
+
+# Add an argument to the init subparser
+init_arg.add_argument(
+    "path",
+    metavar="path",
+    nargs="?",
+    default=".",
+    help="Where to create the repository.")
+
+
+def dit_init(args):
+    """Initialize a new, empty repository.
+    The init command is used to initialize a new, empty repository.
+    Args:
+        path: the path to the directory where the repository will be created.
+    Returns:
+        A new, empty repository.
+    Raises:
+        ValueError: if the repository already exists.
+    Usage:
+        dit init <path>
+        dit init (-h | --help)
+    Options:
+        -h, --help  Show this screen and exit.
+    """
+    try:
+        create_repo(args.path)
+        print(f"Initialized empty dit repository in {args.path}/.git/")
+    except ValueError as err:
+        print(err)
+        sys.exit(1)
