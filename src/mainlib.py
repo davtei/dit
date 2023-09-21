4ab9bc331bd5b428baafae3b09819b0facd493b9
David Tei <daveteidt@gmail.com>
2023-09-21 09:20:39 +0000

    Mute unimplemented functions

--- src/mainlib.py
+++ src/mainlib.py
@@ -4,13 +4,13 @@
 """A module that defines the dit commands."""
 
 # from src.dit_commands.cat_file import dit_cat_file
-from src.dit_commands.checkout import dit_checkout
+# from src.dit_commands.checkout import dit_checkout
 from src.dit_commands.hash_object import dit_hash_object
 from src.dit_commands.init import dit_init
 from src.dit_commands.ls_tree import dit_ls_tree
 from src.dit_commands.show_ref import dit_show_ref
-from src.dit_commands.symbolic_ref import dit_symbolic_ref
-from src.dit_commands.tag import dit_tag
+# from src.dit_commands.symbolic_ref import dit_symbolic_ref
+# from src.dit_commands.tag import dit_tag
 from src.dit_commands.update_ref import dit_update_ref
 from src.parsers import parser
 
@@ -28,9 +28,9 @@ def main(arg=None):
 
     # if command == "cat-file":
     #     dit_cat_file(args)
-    if command == "checkout":
-        dit_checkout(args)
-    elif command == "hash-object":
+    # if command == "checkout":
+    #     dit_checkout(args)
+    if command == "hash-object":
         dit_hash_object(args)
     elif command == "init":
         dit_init(args)
@@ -38,10 +38,10 @@ def main(arg=None):
         dit_ls_tree(args)
     elif command == "show-ref":
         dit_show_ref(args)
-    elif command == "symbolic-ref":
-        dit_symbolic_ref(args)
-    elif command == "tag":
-        dit_tag(args)
+    # elif command == "symbolic-ref":
+    #     dit_symbolic_ref(args)
+    # elif command == "tag":
+    #     dit_tag(args)
     elif command == "update-ref":
         dit_update_ref(args)
     else:
