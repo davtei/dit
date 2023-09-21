2a5c8639780f0eeda4e2dee86fa795bd50ced179
David Tei <daveteidt@gmail.com>
2023-09-21 18:37:22 +0000

    Modify read_object

--- src/objects/read_object.py
+++ src/objects/read_object.py
@@ -5,10 +5,47 @@
 import os
 import zlib
 
+from src.dit_commands.tree_parsing import tree_parse, tree_serialize
 from src.objects.blob_object_class import BlobObject
 from src.objects.commit_object_class import CommitObject
-from src.objects.tree_object_class import TreeObject
+from src.objects.gitobject_class import GitObject
+# from src.objects.tree_object_class import TreeObject
 from src.repos.repo_paths import git_file_path
+
+
+class TreeObject(GitObject):
+    """A class that defines a git tree object,
+    a subclass of the GitObject class.
+    Attributes:
+        object_format: The format of the git object.
+            A git tree object has the format "tree".
+        leaves: The leaves of the git tree object.
+            A leaf is a file or a directory.
+            A leaf is an instance of the GitTreeLeaf class.
+    """
+    object_format = "tree"
+
+    def __init__(self, repo, data=None):
+        """Initialize a git tree object with the provided repo and
+        data (optional).
+        Args:
+            repo (str): The path to the git repository.
+            data (str): The serialized git tree object.
+        """
+        # GitObject.__init__(self, repo, data)
+        super().__init__(repo, data)
+
+    # Convert the tree object to a string representation
+    def serialize(self):
+        """Serialize the git tree object."""
+        return tree_serialize(self)
+
+    # Convert the string representation of a tree object to a tree object,
+    #  an instance of the TreeObject class
+    def deserialize(self, data):
+        """Deserialize the git tree object."""
+        self.leaves = tree_parse(data)
+
 
 
 def read_object(repo, sha):
@@ -78,7 +115,7 @@ def read_object(repo, sha):
         object_classes = {
             b"blob": BlobObject,
             b"commit": CommitObject,
-            b"tree": TreeObject
+            b"tree": TreeObject,
         }
         object_class = object_classes.get(object_format)
         if object_class is None:
